1b0d34f999d4002d614dcd51b3f575a095a6c069
Nassti <nastya@anisimoff.tel>
2022-06-21 15:11:33 +0300

    1

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import { StoreModule } from './store/store.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StoreComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/store/store.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { ModelModule } from "../model/model.module";
+import { CounterDirective } from "./counter.directive";
+
+@NgModule({
+    imports: [ModelModule, BrowserModule, FormsModule],
+    declarations: [CounterDirective],
+    exports: []
+})
+export class StoreModule { }
